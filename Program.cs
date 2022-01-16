f2b453ae0daacd95ca4205223b72269145b6d2f3
Joseph Antonio <joseph.carita@tecsup.edu.pe>
2022-01-16 11:26:01 -0500

    EF Core implementation

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 using Microsoft.OpenApi.Models;
-using PizzaStore.DB;
+using PizzaStore.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
+
 builder.Services.AddSwaggerGen(c =>
   {
       c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Description = "Keep track of your tasks", Version = "v1" });
@@ -20,11 +23,31 @@ app.UseSwaggerUI(c =>
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/pizzas/{id}", async (PizzaDb db, int id) => await db.Pizzas.FindAsync( id ));
 
-app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetOnePizza( id ));
-app.MapGet("/pizzas", () => PizzaDB.GetPizzas());
-app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.createPizza( pizza ));
-app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.updatePizza( pizza ));
-app.MapDelete("/pizzas/{id}",( int id ) => PizzaDB.deletePizza( id ));
+app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
+
+app.MapPost("/pizzas", async (PizzaDb db,Pizza pizza) => {
+  await db.Pizzas.AddAsync( pizza );
+  await db.SaveChangesAsync();
+  return Results.Created($"/pizza/{pizza.Id}",pizza);
+});
+
+app.MapPut("/pizzas/{id}", async (PizzaDb db,Pizza pizzaUpdate, int id) => {
+  var pizza = await db.Pizzas.FindAsync( id );
+  if (pizza == null) return Results.NotFound();
+  pizza.Name = pizzaUpdate.Name;
+  pizza.Description = pizzaUpdate.Description;
+  await db.SaveChangesAsync();
+  return Results.Ok();
+});
+
+app.MapDelete("/pizzas/{id}", async( PizzaDb db,int id ) => {
+  var pizza = await db.Pizzas.FindAsync( id );
+  if (pizza == null) return Results.NotFound();
+  db.Pizzas.Remove( pizza );
+  await db.SaveChangesAsync();
+  return Results.Ok();
+}); 
 
 app.Run();
